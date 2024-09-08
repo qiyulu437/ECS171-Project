2590cc0efb6baa46445478d3c8f13e6488b2d219
GenericSteven <ssyi@ucdavis.edu>
2024-09-08 15:55:51 -0700

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -24,12 +24,39 @@ import numpy as np
 
 
 
-nodes = [16, 32, 64]
-kernel_size = [3, 4, 5, 6, 7, 8]
-pool_size = [3, 4, 5, 6, 7, 8]
-lr = [0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1]
+nodes = [6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40]
+kernel_size = [2, 3, 4, 5]
+pool_size = [3, 4, 5, 6]
+lr = [0.0000001, 0.000001, 0.00001, 0.0001, 0.001, 0.01]
+
+weights = {0: 1.108, 1: 10.226}
 
 f1_score = f1()
+
+def build_model_final(num_nodes, kern, pool, learn):
+    # https://www.tensorflow.org/api_docs/python/tf/keras/preprocessing/image_dataset_from_directory
+    # Generate a tf.data.Dataset object from the 256 folder
+    # Binary labeling of either waldo or not waldo
+    
+    CNN = models.Sequential(name = "Waldo")
+    CNN.add(layers.Input(shape = (256, 256, 3)))
+    CNN.add(layers.Conv2D(num_nodes, (kern, kern), activation = "leaky_relu", padding = "same"))
+    CNN.add(layers.MaxPooling2D((pool, pool)))
+    CNN.add(layers.Conv2D(num_nodes * 2, (kern, kern), activation = "leaky_relu", padding = "same"))
+    CNN.add(layers.MaxPooling2D((pool, pool)))
+    CNN.add(layers.Conv2D(num_nodes * 4, (kern, kern), activation = "leaky_relu", padding = "same"))
+
+    CNN.add(layers.Flatten())
+    #CNN.add(layers.Dense(hp.Choice("nodes", nodes) * 2, activation = "leaky_relu"))
+    #CNN.add(layers.Dense(num_nodes * 2, activation = "leaky_relu"))
+    CNN.add(layers.Dense(1, activation = "sigmoid"))
+    adam = Adam(learning_rate = learn)
+
+    CNN.compile(optimizer = adam, 
+                loss = tf.keras.losses.BinaryCrossentropy(),
+                metrics = ["accuracy", "precision", "recall", f1_score])
+    
+    return CNN
 
 def build_model(hp):
     # https://www.tensorflow.org/api_docs/python/tf/keras/preprocessing/image_dataset_from_directory
@@ -38,17 +65,17 @@ def build_model(hp):
     
     CNN = models.Sequential(name = "Waldo")
     CNN.add(layers.Input(shape = (256, 256, 3)))
-    CNN.add(layers.Conv2D(hp.Choice("nodes", nodes), (hp.Choice("kernel_size", kernel_size), hp.Choice("kernel_size", kernel_size)), activation = "sigmoid", padding = "same", strides = 1))
+    CNN.add(layers.Conv2D(hp.Choice("nodes", nodes), (hp.Choice("kernel_size", kernel_size), hp.Choice("kernel_size", kernel_size)), activation = "leaky_relu", padding = "same"))
     CNN.add(layers.MaxPooling2D((hp.Choice("pool_size", pool_size), hp.Choice("pool_size", pool_size))))
-    CNN.add(layers.Conv2D(hp.Choice("nodes", nodes) * 2, (hp.Choice("kernel_size", kernel_size), hp.Choice("kernel_size", kernel_size)), activation = "sigmoid", padding = "same", strides = 1))
+    CNN.add(layers.Conv2D(hp.Choice("nodes", nodes) * 2, (hp.Choice("kernel_size", kernel_size), hp.Choice("kernel_size", kernel_size)), activation = "leaky_relu", padding = "same"))
     CNN.add(layers.MaxPooling2D((hp.Choice("pool_size", pool_size), hp.Choice("pool_size", pool_size))))
-    CNN.add(layers.Conv2D(hp.Choice("nodes", nodes) * 4, (hp.Choice("kernel_size", kernel_size), hp.Choice("kernel_size", kernel_size)), activation = "sigmoid", padding = "same", strides = 1))
+    CNN.add(layers.Conv2D(hp.Choice("nodes", nodes) * 4, (hp.Choice("kernel_size", kernel_size), hp.Choice("kernel_size", kernel_size)), activation = "leaky_relu", padding = "same"))
 
     CNN.add(layers.Flatten())
-    CNN.add(layers.Dense(hp.Choice("nodes", nodes) * 4, activation = "sigmoid"))
-    CNN.add(layers.Dense(hp.Choice("nodes", nodes) * 2, activation = "sigmoid"))
+    #CNN.add(layers.Dense(hp.Choice("nodes", nodes) * 2, activation = "leaky_relu"))
+    #CNN.add(layers.Dense(hp.Choice("nodes", nodes), activation = "leaky_relu"))
     CNN.add(layers.Dense(1, activation = "sigmoid"))
-    adam = Adam(learning_rate = hp.Choice("learning rate", lr))
+    adam = Adam(learning_rate = hp.Choice("learning_rate", lr))
 
     CNN.compile(optimizer = adam, 
                 loss = tf.keras.losses.BinaryCrossentropy(),
@@ -56,12 +83,12 @@ def build_model(hp):
     
     return CNN
 
-def tune_hyperparameters(training, testing, cv):
-    #build_model(kert.HyperParameters())
+def tune_hyperparameters(training, testing, trials):
     tuner = kert.RandomSearch(build_model,
                               objective = kert.Objective("val_f1_score", direction = "max"),
-                              max_trials = cv, 
-                              seed = 123)
+                              max_trials = trials, 
+                              seed = 123,
+                              project_name = "CNN Tuning")
 
     x = []
     y = []
@@ -83,8 +110,17 @@ def tune_hyperparameters(training, testing, cv):
     x_test = np.concatenate(x, axis = 0)
     y_test = np.concatenate(y, axis = 0)
             
-    tuner.search(x_train, y_train, epochs = 16, validation_data = [x_test, y_test])
-    best = tuner.get_best_models()[0]
+    tuner.search(x_train, 
+                 y_train, 
+                 epochs = 100, 
+                 validation_data = [x_test, y_test],
+                 class_weight = weights)
+    
+    tuner.results_summary(num_trials = 20)
+    '''best_model = tuner.get_best_models()[0]
+    best_params = tuner.get_best_hyperparameters(num_trials = 1)[0]
+    for key, value in best_params.values.items():
+        print(f"{key}: {value}")'''
 
 
 
@@ -94,19 +130,38 @@ original_images = r"original-images"
 keras.backend.clear_session()
 
 training, testing = idft(waldo_images, 
-                         validation_split = 0.2, 
+                         validation_split = 0.3, 
                          subset = "both", 
                          label_mode = "binary", 
                          seed = 123, 
                          labels = "inferred", 
                          image_size = (256, 256), 
-                         batch_size = 5)
-
-
-tune_hyperparameters(training, testing, cv = 5)
+                         batch_size = 10)
 
 
 
+tune_hyperparameters(training, testing, trials = 100)
+CNN = build_model_final(18, 
+                        5, 
+                        3, 
+                        0.1)
+
+'''CNN.fit(training, 
+        epochs = 100)
+
+print("EVALUATION")
+CNN.evaluate(testing)'''
+
+
+'''
+Trial 29 summary
+Hyperparameters:
+nodes: 18
+kernel_size: 5
+pool_size: 3
+learning_rate: 0.1
+Score: 0.4285714030265808
+'''
 
 
 
